df1618d867f629af3abb566ccefbdcc73d748260
hexiaobop <absod0711@163.com>
2015-07-23 21:32:48 +0800

    document finish

--- document/gen/com/example/document/R.java
+++ document/gen/com/example/document/R.java
@@ -1093,7 +1093,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050041;
+        public static final int action_settings=0x7f050042;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1145,6 +1145,7 @@ containing a value of this type.
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int tv_file_info=0x7f050040;
+        public static final int tv_file_size=0x7f050041;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
--- document/src/com/example/document/MainActivity.java
+++ document/src/com/example/document/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.document;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,12 +10,16 @@ import java.util.Map;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private ListView list;
@@ -32,8 +37,56 @@ public class MainActivity extends Activity {
 		list = (ListView) findViewById(R.id.sd_files);
 		btn = (Button) findViewById(R.id.btn_back);
 		tv = (TextView) findViewById(R.id.sd_path);
+		havefile("/");
+		
+		
+		list.setOnItemClickListener(new OnItemClickListener() {
 
-		File root = new File("/mnt");
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				TextView  t = (TextView) arg1.findViewById(R.id.tv_file_info);
+				if (currentFiles[arg2].isFile())
+				{
+					return;
+				}
+				File[] tmp = currentFiles[arg2].listFiles();
+				if (tmp == null || tmp.length == 0)
+				{
+					Toast.makeText(MainActivity.this, "空文件夹", Toast.LENGTH_SHORT).show();
+				} else
+				{
+					currentParent = currentFiles[arg2];
+					currentFiles = tmp;
+					firstlist(currentFiles);
+				}
+			}
+		});
+		btn.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+					try {
+						if(!currentParent.getCanonicalPath().equals("/"))
+						{
+							currentParent = currentParent.getParentFile();
+							currentFiles = currentParent.listFiles();
+							firstlist(currentFiles);
+						}
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				
+			
+			}});
+	}
+	public void havefile(String path)
+	{
+		File root = new File(path);
 		if (root.exists()) {
 			
 			currentParent = root;
@@ -44,7 +97,6 @@ public class MainActivity extends Activity {
 			System.out.println("no  found sdcard");
 		}
 	}
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -61,6 +113,7 @@ public class MainActivity extends Activity {
 				map.put("icon", R.drawable.wenjianjia);
 			} else {
 				map.put("icon", R.drawable.wenjian);
+				
 			}
 			map.put("fileName", f.getName());
 			firstlist.add(map);
@@ -70,7 +123,19 @@ public class MainActivity extends Activity {
 				R.layout.ilist_item, new String[] { "icon", "fileName" }, new int[] {
 						R.id.img_file_icon, R.id.tv_file_info });
 		list.setAdapter(simplea);
+		try
+		{
+			tv.setText("当前路径为： "+currentParent.getCanonicalPath());
+		} catch (Exception e)
+		{
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
 
 	}
+
+
+
 
 }
